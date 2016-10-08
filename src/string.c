17a166cbbeee1cd112a4fcfd9534bcbe06bc383a
Sabita Nayak <simplesabita@gmail.com>
2016-10-08 23:00:19 +0530

    directory changes to src

--- src/string.c
+++ src/string.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int my_strlen(char *s)
+size_t my_strlen(char *s)
 {
    char *p = s;
 
@@ -8,6 +8,7 @@ int my_strlen(char *s)
       ++p;
 
    return (p - s);
+   if else
 }
 
 int main(void)
